a1e4d667fc23568050b5053aaff0df52ed3c9b84
Patrick Austin <patrick.austin@stfc.ac.uk>
2025-01-08 17:02:58 +0000

    flake8:  ->  #86

--- larch_select_paths/larch_select_paths.py
+++ larch_select_paths/larch_select_paths.py
@@ -186,7 +186,7 @@ class GDSWriter:
         else:
             auto_name = self.default_properties[property_name]["name"]
             value = self.default_properties[property_name]["value"]
-            if auto_name == "degen_variable" and value == None:
+            if auto_name == "degen_variable" and value is None:
                 value = feff_degen_value
 
             if directory_label:
